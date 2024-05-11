a3336b2508a2759c640f592489dcba2b671c9f46
ortiqovdev <akmalbekortiqov7976@gmail.com>
2024-05-12 02:27:45 +0500

    optimize app

--- public/js/modelPage.js
+++ public/js/modelPage.js
@@ -1,17 +1,24 @@
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
+let body = document.querySelector('body')
+
 
 btn.onclick = function() {
   modal.style.display = "block";
+  body.style.overflow = "hidden"
 }
 
 span.onclick = function() {
   modal.style.display = "none";
+  body.style.overflow = "auto"
+
 }
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  body.style.overflow = "auto"
+
   }
 }
